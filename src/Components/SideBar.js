a39b6984b0ec4b99b26cdc35c1137e814ae0c2f9
sanjeeva-rao <sanjeevarao436@gmail.com>
2025-01-13 19:02:16 +0530

    added side-menu

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,6 +1,21 @@
 const SideBar = () => {
-    return <div>
-        Side Bar
+    return <div className="p-4 shadow-lg h-screen">
+        <div>
+            <img alt="home-icon" src="https://tse4.mm.bing.net/th?id=OIP.gUFOcWkBJqJzZ6xCyf3EswHaHM&pid=Api&P=0&h=180" className="h-8 w-8"/>
+            <div>Home</div>
+        </div>
+        <div className="py-8">
+            <img alt="shorts-icon" src="https://img.icons8.com/?size=48&id=ajczeHCWXbyR&format=png" className="h-8 w-8" />
+            <div>shorts</div>
+        </div>
+        <div>
+            <img alt="subscription-icon" src="https://tse2.mm.bing.net/th?id=OIP.XvY6LA8rPbB-skw9mjqnyQAAAA&pid=Api&P=0&h=180" className="h-8 w-8"/>
+            <div>Subscriptions</div>
+        </div>
+        <div className="py-8">
+            <img alt="user-icon" src="https://tse1.mm.bing.net/th?id=OIP.GJf5dUKrx-14LSV-54G8QAHaHa&pid=Api&P=0&h=180" className="h-8 w-8"/>
+            <div>You</div>
+        </div>
     </div>
 }
 export default SideBar;
